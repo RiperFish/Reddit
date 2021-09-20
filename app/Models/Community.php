721ef99c5327753d8babdb4e4a53fd5d6e1ac61c
RiperFish <mustapha.benfetta@gmail.com>
2021-09-20 19:45:56 +0100

    added slug to communities using : eloquent sluggable package

--- app/Models/Community.php
+++ app/Models/Community.php
@@ -5,23 +5,35 @@ namespace App\Models;
 use App\Models\Post;
 use App\Models\Topic;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Community extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory, SoftDeletes, Sluggable;
 
     protected $fillable = [
         'user_id',
         'name',
         'description',
+        'slug'
     ];
 
-    public function topics(){
+    public function topics()
+    {
         return $this->belongsToMany(Topic::class);
     }
-    public function posts(){
+    public function posts()
+    {
         return $this->hasMany(Post::class);
+    }
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
     }
 }
